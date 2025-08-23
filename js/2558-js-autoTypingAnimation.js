097603fffebdf624789c17a56d9417734455255d
Md Robiul Islam <148268570+roobiul@users.noreply.github.com>
2025-08-23 21:09:23 +0600

    first
    first in 23-8-25...9.09pm

---
+++ js/2558-js-autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span> Hi, I am Robiul Islam</span>  A CSE Student , designer,and problem-solver <br><br> I explore what makes humans tick,keep learning,testing, and improving."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
